075f3ad59953096e875137566ca8591ad6ac465d
imagetech <paul@deaddroplabs.com>
2022-05-09 16:48:47 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,37 +1,47 @@
-
 #!/usr/bin/env python3
+
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 # load our env file
-print ('Loading env file')
-load_dotenv()
+#print ('Loading env file')
+#load_dotenv()
 
 import aws_cdk as cdk
 
+
 from ec2_deploy.ec2_deploy_stack import Ec2WorkstationDeployStack
 from ec2_deploy.ec2_portal_deploy import Ec2DeployPortalStack
+from ec2_deploy.ec2_soda_conductor_deploy import Ec2DeployConductorStack
 from ec2_deploy.ec2_soda_agent_deploy import Ec2DeploySoDAClientStack
 
 print ('Creating environment')
 account=os.getenv('CDK_DEFAULT_ACCOUNT')
-region=os.getenv('CDK_DEFAULT_REGION')
+region=os.getenv('CDK_TARGET_REGION')
 
-cdk_env = cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION'))
 
 print("Deploy to:" + str(account) + ":" + str(region))
+
+cdk_env = cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=region)
+print("app.py cdk_env:" + str(cdk_env))
+
+print("Deploy to:" + str(account) + ":" + str(region))
+
 
 # deploy reources
 app = cdk.App()
 
 # admin web portal
-Ec2DeployPortalStack(app, 'Ec2DeployPortalStack', env=cdk_env)
+Ec2DeployConductorStack(app, 'Ec2DeployConductorStack',  env=cdk_env)
+
+# admin web portal
+#Ec2DeployPortalStack(app, 'Ec2DeployPortalStack',  env=cdk_env)
 
 # SoDA client server (ARM)
-Ec2DeploySoDAClientStack(app, 'Ec2DeploySoDAClientStack', env=cdk_env)
+#Ec2DeploySoDAClientStack(app, 'Ec2DeploySoDAClientStack', env=cdk_env )
 
 # a workstation
-Ec2WorkstationDeployStack(app, 'Ec2WorkstationDeployStack', env=cdk_env)
+#Ec2WorkstationDeployStack(app, 'Ec2WorkstationDeployStack', env=cdk_env)
 
 
 
